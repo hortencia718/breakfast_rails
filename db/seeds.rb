f6a278e7f7a66ff0fe6fbfdc65856afed021b8af
Elronia <zar.catherine@gmail.com>
2020-08-29 14:50:08 -0400

    adding some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+dog_1 = Dog.create(name: "Coco", age: 5)
+
+activity_1 = Activity.create(name: "Swimming", duration: 30)
+
+daycare_1 = Daycare.create(dog_id: 1, activity_id: 1)
