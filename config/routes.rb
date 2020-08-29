25fbc2c479db0c0348b9a6ce9e12945643ab9ee2
Elronia <zar.catherine@gmail.com>
2020-08-29 17:47:08 -0400

    creating new routes for dogs

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/dogs", to: "dogs#index", as: "dogs"
   get "/dogs/:id", to: "dogs#show", as: "dog"
+  get "/dogs/new", to: "dogs#new", as: "new_dog"
+  post "/dogs", to: "dogs#create"
+
+
   get "/activities", to: "activities#index", as: "activities"
   get "/activities/:id", to: "activities#show", as: "activity"
+  
   
 end
