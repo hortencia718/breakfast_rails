05f87618839f37a2c88809e2b9220802c1a8a857
Elronia <zar.catherine@gmail.com>
2020-08-29 17:26:41 -0400

    writing index page for activities

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/dogs", to: "dogs#index", as: "dogs"
   get "/dogs/:id", to: "dogs#show", as: "dog"
+  get "/activities", to: "activities#index", as: "activities"
+  get "/activities/:id", to: "activities#show", as: "activity"
   
 end
